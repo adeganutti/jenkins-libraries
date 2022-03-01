a12d650c6d05b0ff5f6d66a0c95db4ae7da0d154
adeganutti <83947609+adeganutti@users.noreply.github.com>
2022-03-01 19:09:19 +0100

    Update sonarqube.groovy

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -1,6 +1,7 @@
 def call(sonarqubeEnv, credentialsId) {
-	echo sonarqubeEnv
-	echo credentialsId
+	//echo sonarqubeEnv
+	//echo credentialsId
+	
 	withSonarQubeEnv(sonarqubeEnv) {
 		withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'SONAR_USERNAME', passwordVariable: 'SONAR_PASSWORD')]) {
 			sh "mvn clean verify sonar:sonar -Dsonar.login=$SONAR_USERNAME -Dsonar.password=$SONAR_PASSWORD"
