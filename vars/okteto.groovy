6b701ca81db90f508674b319888c64a5f31993dc
adeganutti <83947609+adeganutti@users.noreply.github.com>
2022-03-02 10:12:16 +0100

    Update okteto.groovy

--- vars/okteto.groovy
+++ vars/okteto.groovy
@@ -1,12 +1,11 @@
 def call(oktetoServerUrl, oktetoGithubRepositoryName, oktetoGithubRepositoryUrl, oktetoDeploymentName, oktetoCredentialId){
   echo "Deploying on Okteto server : ${oktetoServerUrl} - ${oktetoGithubRepositoryName} - ${oktetoGithubRepositoryUrl} - ${oktetoDeploymentName}"
   
-  /*
   withCredentials([string(credentialsId: oktetoCredentialId, variable: 'TOKEN')]) {
     sh "okteto context use ${oktetoServerUrl} --token $TOKEN"
     sh "okteto kubeconfig"
     sh "okteto pipeline destroy -p ${oktetoGithubRepositoryName} -w -l info"
     sh "okteto pipeline deploy -r ${oktetoGithubRepositoryUrl} -b main -w -l info"
     sh "kubectl rollout restart deployment ${oktetoDeploymentName}"
-  }*/
+  }
 }
