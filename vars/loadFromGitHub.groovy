e736cb1fdff5961cfc44695623e5611c9f957e39
adeganutti <83947609+adeganutti@users.noreply.github.com>
2022-03-02 15:46:27 +0100

    Create loadFromGitHub.groovy

---
+++ vars/loadFromGitHub.groovy
@@ -0,0 +1,20 @@
+def call(serverHost, organization, repository, filePath, credentials) {
+	try
+	{
+		withCredentials([usernamePassword(credentialsId: credentials, usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')])
+		{
+			def scriptTempName = new Date().getTime().toString()
+			def b64 = json(shout("curl -u ${USERNAME}:${PASSWORD} -H 'Accept: application/vnd.github.v3+json' 'https://${serverHost}/api/v3/repos/${organization}/${repository}/contents/${filePath}'"))?.content?.replace("\n","")
+			if(b64 == null)
+				error "ERROR getFileFromGitHub: invalid groovy file."
+			writeFile text: b64, file: scriptTempName, encoding: "Base64"
+			def res = load scriptTempName
+			return res
+		}
+	}
+	catch(e)
+	{
+		println "ERROR loadFromGitHub: ${e.message}"
+		return null
+	}
+}
